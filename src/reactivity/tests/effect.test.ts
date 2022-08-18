781f831d5cce6b09bf854830244a37d52009dd3d
wangbaishun <863548227@qq.com>
2022-08-18 09:43:43 +0800

    feat:jext环境搭建

---
+++ src/reactivity/tests/effect.test.ts
@@ -0,0 +1,16 @@
+import {add} from '../../index'
+describe('effect',() => {
+
+    test("effect", () => {
+        const user = reactive({
+            age:10
+        })
+        let nextage
+        effect(() => {
+        nextage=user.age+1
+        })
+        expect(nextage).toBe(11)
+        user.age = 20
+        expect(nextage).toBe(21)
+    })
+})
